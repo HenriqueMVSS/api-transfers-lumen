0b138ab284e47098df0f58768c7d62cedcc4ff39
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2023-05-12 17:30:40 -0300

    configurated stark bank v2 api

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -3,12 +3,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use StarkBank\Transfer;
+use StarkBank\Project;
 use StarkBank\Settings;
 
 class TransferController extends Controller
 {
     public function __construct() {
-        Settings::setUser(env('STARKBANK_USER'));
+        $privateKeyContent = "
+        -----BEGIN EC PRIVATE KEY-----
+        MHQCAQEEIFzE9cS4LV6NeEGCE8P9QRWB9YCr0+MkncNJz7Vixp3UoAcGBSuBBAAK
+        oUQDQgAEalGaJ1m9bMKSq8zIZX/2FQbJQw17s6L0TkUObvHxXaUwNw9OStkbP3vY
+        w2WJG1ljOjU2hIB1OtCETHP+JEdAZQ==
+        -----END EC PRIVATE KEY-----
+        ";
+
+        $user = new Project([
+            "environment" => env('STARKBANK_ENVIRONMENT'),
+            "id" => env('STARKBANK_ID'),
+            "privateKey" => $privateKeyContent
+        ]);
+        Settings::setUser($user);
     }
 
     public function index()
